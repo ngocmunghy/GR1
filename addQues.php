017fb9ad3943702058e0a48981fcb6fc61c7366f
ngocmunghy <mung09021999@gmail.com>
2021-05-26 03:30:06 +0700

    add question form done

--- addQues.php
+++ addQues.php
@@ -7,14 +7,66 @@
       </div>
       <div class="modal-body">
         <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          <label for="question-area" class="form-label">Question</label>
+          <textarea class="form-control" id="question-area" rows="3" placeholder="Input new question here ..."></textarea>
         </div>
         <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <label for="optA" class="form-label">Option A</label>
+          <textarea class="form-control" id="optA" rows="1"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="optB" class="form-label">Option B</label>
+          <textarea class="form-control" id="optB" rows="1"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="optC" class="form-label">Option C</label>
+          <textarea class="form-control" id="optC" rows="1"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="optD" class="form-label">Option D</label>
+          <textarea class="form-control" id="optD" rows="1"></textarea>
+        </div>
+
+        <div class = "form-group">
+          <label class="form-label">Correct answer</label>
+          <div class="row">
+
+            <div class="col-sm-3">
+              <div class="radio">
+                <label>
+                  <input type="radio" name="ans"> Option A
+                </label>
+              </div>
+            </div>
+
+            <div class="col-sm-3">
+              <div class="radio">
+                <label>
+                  <input type="radio" name="ans"> Option B
+                </label>
+              </div>
+            </div>
+
+            <div class="col-sm-3">
+              <div class="radio">
+                <label>
+                  <input type="radio" name="ans"> Option C
+                </label>
+              </div>
+            </div>
+
+            <div class="col-sm-3">
+              <div class="radio">
+                <label>
+                  <input type="radio" name="ans"> Option D
+                </label>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
+
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
