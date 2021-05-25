2d317fcd8a4b09367814a92cb7a5824e55cda40b
ngocmunghy <mung09021999@gmail.com>
2021-05-26 02:58:30 +0700

    add modal when user click to add's button

---
+++ addQues.php
@@ -0,0 +1,24 @@
+<div class="modal" tabindex="-1" role = "dialog" id = "mdlQuestion">
+  <div class="modal-dialog" role = "document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">New question</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-	include("connect.php")
+	include("connect.php");
+	include("addQues.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,44 +9,53 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title></title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<div class="container">
-			<table class="table caption-top table-striped table-hover">
-				<caption>List of users</caption>
+			<table class="table table-striped table-hover">
 				<thead>
 					<tr>
 						<th scope="col">ID</th>
 						<th scope="col">Question</th>
-						<th scope="col">Option A</th>
-						<th scope="col">Option B</th>
-						<th scope="col">Option C</th>
-						<th scope="col">Option D</th>
-						<th scope="col">Answer</th>
+						<th scope="col">Action</th>
 					</tr>
 				</thead>
+
 				<tbody>
+					<div class="d-flex justify-content-end">
+						<button id = "addQuestion" class="btn btn-success">+</button>
+					</div>
 					<?php
+						
 						$sql 	= "SELECT * FROM question";
 						$result = $conn->query($sql);
 						
 						while($row = $result->fetch_assoc()) {
-
 							echo "<tr>";
-							echo "<th scope = 'row'>" . $row["id"] . "</th>";
-							echo "<th scope = 'row'>" . $row["description"] . "</th>";
-							echo "<th scope = 'row'>" . $row["option_a"] . "</th>";
-							echo "<th scope = 'row'>" . $row["option_b"] . "</th>";
-							echo "<th scope = 'row'>" . $row["option_c"] . "</th>";
-							echo "<th scope = 'row'>" . $row["option_d"] . "</th>";
-							echo "<th scope = 'row'>" . $row["answer"] . "</th>";
+									echo "<td scope = 'row'>" . $row["id"] . "</td>";
+									echo "<td class = 'text-primary'>" . $row["description"] . "</td>";
+									echo "<td scope = 'row'>";
+									echo "<a class = 'btn btn-sm btn-warning' href = '#' role = 'button'>View</a> &nbsp";
+									echo "<a class = 'btn btn-sm btn-secondary' href = '#' role = 'button'>Edit</a> &nbsp";
+									echo "<a class = 'btn btn-sm btn-danger' href = '#' role = 'button'>Delete</a> &nbsp";
+									echo "</td>";
 							echo "</tr>";
-
 						}
 					?>
 				</tbody>
 			</table>
 		</div>
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$('#addQuestion').click(function() {
+					$('#mdlQuestion').modal('show');
+				});
+			});
+			
+		</script>
 	</body>
 </html>
